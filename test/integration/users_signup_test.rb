4ac00f3860e152f9dbf9660731531f0f78cbc17f
Wojciech Szymanski <kwcontact@yahoo.com>
2015-04-01 18:07:12 +0200

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		                                        password_confirmation: "password" }
 		end
 		assert_template 'users/show'
+		assert is_logged_in?
+
 	end
 
 end
